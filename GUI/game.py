4d7f855ec2d4aa84e1e06ae79cb92cd2c9ae97e6
junchh <cjunho9868@gmail.com>
2020-11-27 18:58:42 +0700

    reading input working

--- GUI/game.py
+++ GUI/game.py
@@ -42,7 +42,19 @@ def drawNumber(num, x, y):
         pygame.draw.rect(DISPLAYSURF, DARKGREY, (x * 50 + 3, y * 50 + 3, 47, 47))
         DISPLAYSURF.blit(text, [50 * x + 17, 50 * y + 12])
 
-siz = 5
+
+file_input = open('input.txt')
+lines = file_input.readlines()
+
+siz = int(lines[0])
+
+mat1 = [[0 for j in range(siz)] for i in range(siz)]
+mat_status = [[0 for j in range(siz)] for i in range(siz)]
+
+for i in range(2, len(lines)):
+    a = lines[i].split(", ")
+    mat1[int(a[0])][int(a[1])] = 1
+
 def createMat(mat):
     mat_baru = [[0 for j in range(siz)] for i in range(siz)]
     for i in range(siz):
@@ -62,20 +74,44 @@ def createMat(mat):
     return mat_baru
 
 
-
-
-mat1 = [[0, 0, 0, 0, 1], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0], [0, 1, 1, 0, 1], [0, 0, 1, 0, 1]]
-mat_status = [[0 for j in range(siz)] for i in range(siz)]
 rep_mat = createMat(mat1)
 
-def doother():
+for i in range(siz):
+    for j in range(siz):
+        print(rep_mat[i][j], end=" ")
+    print()
+
+
+
+
+
+# x,y harus belum dibuka
+def openCell(i, j):
+    if(rep_mat[i][j] > 0):
+        print("hihi ", i, j)
+        mat_status[i][j] = 1
+    elif (rep_mat[i][j] == 0):
+        mat_status[i][j] = 1
+        for k in range(8):
+            newi = i + mov[k][0]
+            newj = j + mov[k][1]
+            if(newi >= 0 and newi < siz and newj >= 0 and newj < siz and mat_status[newi][newj] == 0):
+                openCell(newi, newj) 
+
+
+def updateDisplay():
     for i in range(siz):
         for j in range(siz):
-            time.sleep(1)
-            drawNumber(rep_mat[i][j], i, j)
+            if(mat_status[i][j] == 1):
+                drawNumber(rep_mat[i][j], j, i)
 
-x = threading.Thread(target=doother, args=())
-x.start()
+
+# def doother():
+#     openCell(0, 0)
+#     updateDisplay()
+
+# x = threading.Thread(target=doother, args=())
+# x.start()
 
 
 # Beginning Game Loop
@@ -89,7 +125,10 @@ while True:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = event.pos 
-
+            x = mouse_pos[0] // 50
+            y = mouse_pos[1] // 50
+            openCell(y, x)
+            updateDisplay()
             print(mouse_pos)
 
 
